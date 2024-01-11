3339345bd472ddcf825e6e31529d131b01c13df6
jx1820@ic.ac.uk <jx1820@ic.ac.uk>
2024-01-11 23:26:18 +0000

    commit with current complete BNF and error annoate added

--- qsofinstr/parser.py
+++ qsofinstr/parser.py
@@ -32,12 +32,66 @@ class Parser(Token):
         node.val = val
         return node
     
+    ### Recursive descent parsing ###
     ''' BNF of modified OpenQASM
     mainprogram := OPENQASM real; program
-    program := statement | program statement
-    statement := decl 
-    | gatedecl goplist }
+    program     := statement | program statement
+    statement   := decl 
+                   | gatedecl goplist }
+                   | gatedecl }
+                   | opaque id idlist ;
+                   | opaque id () idlist ; | opaque id (idlist) idlist ;
+                   | qop
+                   | if (id == nninteger) qop
+
+    decl        := qreg id [nninteger] ; | creg id [nninteger] ;
+    gatedecl    := gate id idlist {
+                   | gate id () idlist {
+                   | gate id (idlist) idlist {
+    goplist     := uop 
+                   | goplist uop 
+    qop         := uop 
+                   | measure argument -> argument ;
+                   | reset argument ;
+    uop         := U (explist) argument ;
+                   | CX argument , argument ;
+                   | X argument ;
+                   | Y argument ;
+                   | Z argument ;
+                   | S argument ;
+                   | T argument ;
+                   | RTHETA (explist) argument ;
+                   | RX (explist) argument
+                   | RY (explist) argument
+                   | RZ (explist) argument
+                   | H argument ;
+                   | CY argument, argument ;
+                   | CZ argument, argument ;
+                   | CU (explist) argument, argument ;
+                   | CS argument, argument ;
+                   | CT argument, argument ;
+                   | CRTHETA (explist) argument, argument ;
+                   | CRX (explist) argument, argument ;
+                   | CRY (explist) argument, argument ;
+                   | CRZ (explist) argument, argument ;
+                   | CH argument, arugment ;
+                   | id anylist ; | id () anylist ;
+                   | id (explist) anylist ;
+    anylist     := idlist | mixedlist
+    idlist      := id | idlist , id
+    mixedlist   := id [nninteger] | mixedlist , id
+                   | mixedlist, id [nninteger]
+                   | idlist , id [nninteger]
+    argument    := id | id [nninteger]
+    explist     := exp | explist , exp
+    exp         := real | nninteger | pi | id
+                   | exp + exp | exp - exp | exp * exp 
+                   | exp / exp | - exp | exp ^ exp
+                   | (exp) | unaryop (exp)
+    unaryop     := sin | cos | tan | exp | ln | sqrt
+    
+    id          := [a-z][A-Za-z0-9_]*
+    real        := ([0-9]+\.[0-9]*|[0-9]*\.[0-9]+)([eE][-+]?[0-9]+)?
+    nninteger   := [1-9]+[0-9]*|0
+                   
     '''
-    
-    
-        
